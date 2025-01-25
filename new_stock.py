c496cb4e044f8dd6d178202b6e75cdfedf127552
uki1998 <kalember98@gmail.com>
2025-01-25 18:34:39 +0100

    new_stock.py

--- new_stock.py
+++ new_stock.py
@@ -19,11 +19,11 @@ def calculate_stock_growth_with_dividends(
 
     return investment_value
 
-initial_investment = 10000  # initial investment in SEK
-annual_dividend_yield = 0.13  # annual dividend yield
-dividend_payouts_per_year = 12  # amount of dividend payouts/year
-annual_stock_price_change = 0.05  # annual growth in stock price
-years_held = 3  # holding period in years
+initial_investment = x  # initial investment in SEK
+annual_dividend_yield = x  # annual dividend yield (apply decimally, for example 10% should be 0.1)
+dividend_payouts_per_year = x  # amount of dividend payouts/year
+annual_stock_price_change = x  # annual growth in stock price (apply decimally)
+years_held = x  # holding period in years
 
 # calculate final value
 final_value = calculate_stock_growth_with_dividends(
